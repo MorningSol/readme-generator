2c42f1886540e69dcfdfd4327702bcb7fa1eb0e3
MorningSol <98545619+MorningSol@users.noreply.github.com>
2022-04-15 14:05:38 -0400

    inquirer set up and first question created.  commented out badge link for futur use

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs')
+
 
 // TODO: Create an array of questions for user input
 const questions = [];
+
+const promptUser = () => {
+    return inquirer.prompt([
+       {
+           type: 'input',
+           name: 'username',
+           message: 'Enter your GitHub user name.'
+       } 
+    ])
+    
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -11,3 +25,11 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+
+
+promptUser().then(answers => console.log(answers));
+
+
+// https://img.shields.io/badge/<LABEL>-<MESSAGE>-<COLOR> 
+// https://img.shields.io/badge/license-MIT-brightgreen?style=plastic
